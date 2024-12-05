ab4abb22ea6b6f248f19c6da85cbb823e0f68198
yonatanhavasov <yonihavasov@gmail.com>
2024-12-05 15:16:26 +0200

    badger up-mysql

--- docker-curriculum/flask-app/app.py
+++ docker-curriculum/flask-app/app.py
@@ -1,24 +1,46 @@
 from flask import Flask, render_template
 import os
+import mysql.connector
 import random
 
 app = Flask(__name__)
 
-# list of cat images
-images = [
-    "https://media.tenor.com/zZ1i8J3C2RUAAAAM/hello-robert-e-fuller.gif",
-    "https://media.tenor.com/1y8rq5FE6OEAAAAM/yawning-robert-e-fuller.gif",
-    "https://media.tenor.com/ohykMcgIpk8AAAAM/badgers-badgertime.gif",
-    "https://media.tenor.com/KYE8V6nrzzYAAAAM/itchy-robert-e-fuller.gif",
-    "https://media.tenor.com/YcZbpukPl34AAAAM/honey-badger-dance.gif"
-]
+# Database connection details
+db_config = {
+    'host': 'db',  # Name of the MySQL service in docker-compose.yaml
+    'user': 'gif_user',
+    'password': 'gif_password',
+    'database': 'gif_db'
+}
 
+# Function to get image URLs from the database
+def get_image_urls():
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        # Query to get all image URLs from the 'images' table
+        cursor.execute('SELECT url FROM images')
+        image_urls = [row[0] for row in cursor.fetchall()]
+
+        cursor.close()
+        conn.close()
+
+        return image_urls
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return []
 
 @app.route("/")
 def index():
-    url = random.choice(images)
-    return render_template("index.html", url=url)
+    image_urls = get_image_urls()
 
+    # Check if there are image URLs in the database
+    if image_urls:
+        url = random.choice(image_urls)
+        return render_template("index.html", url=url)
+    else:
+        return "No images found in the database."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
