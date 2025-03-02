b7b7ca7b9bc5cecf3c1e76feb6aec8d0b3b36a23
yonatanhavasov <yonihavasov@gmail.com>
2025-03-02 13:54:37 +0200

    Delete .history directory

--- .history/flask-app/terraform-gke-k8s/outputs_20250210221405.tf
+++
@@ -1,7 +0,0 @@
-output "gke_cluster_name" {
-  value = google_container_cluster.gke_cluster.name
-}
-
-output "mysql_instance_name" {
-  value = google_sql_database_instance.mysql.name
-}
--- .history/flask-app/terraform-gke-k8s/outputs_20250210233851.tf
+++
@@ -1,3 +0,0 @@
-output "gke_cluster_name" {
-  value = google_container_cluster.gke_cluster.name
-}
