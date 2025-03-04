af28038d2d64c750b4ba9fc234a967dc5fa9faa8
yonatanhavasov <yonihavasov@gmail.com>
2025-03-04 11:56:17 +0200

    gif add

--- flask-app/init.sql
+++ flask-app/init.sql
@@ -13,7 +13,10 @@ INSERT INTO images (url) VALUES
 ('https://media.tenor.com/1y8rq5FE6OEAAAAM/yawning-robert-e-fuller.gif'),
 ('https://media.tenor.com/KYE8V6nrzzYAAAAM/itchy-robert-e-fuller.gif'),
 ('https://media.tenor.com/YcZbpukPl34AAAAM/honey-badger-dance.gif'),
-('https://media1.tenor.com/m/uy06yNc_aXQAAAAd/honey-badger-woah.gif');
+('https://media1.tenor.com/m/uy06yNc_aXQAAAAd/honey-badger-woah.gif'),
+('https://media1.tenor.com/m/kAvWsWS8vZgAAAAd/putois-carmaux.gif'),
+('https://media1.tenor.com/m/TAf_lYi62gsAAAAd/dont-care-honey-badger.gif'),
+('https://media1.tenor.com/m/U4_Uag4pZLsAAAAC/thug-life-honey-badger.gif');
 
 CREATE TABLE IF NOT EXISTS visitor_counter (
     id INT AUTO_INCREMENT PRIMARY KEY,
